5f27456c15f4f1e7208b58d6a5b3436cec99153c
ZèD <imzihad@gmail.com>
2022-04-25 01:02:00 +0600

    layout: added layout with navbar and footer
        * Also formatted the code to make it more readable
    
    Signed-off-by: ZèD <imzihad@gmail.com>

--- configs/theme.ts
+++ configs/theme.ts
@@ -1,15 +1,15 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 // Create a theme instance.
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#556cd6',
-        },
-        secondary: {
-            main: '#19857b',
-        },
+  palette: {
+    primary: {
+      main: "#556cd6",
     },
+    secondary: {
+      main: "#19857b",
+    },
+  },
 });
 
 export default theme;
