9ce8ada94129b3152dd7b96d00f1c512da63e4d7
Mofajjal Rasul <imzihad@gmail.com>
2022-04-27 14:29:07 +0600

    mod: moved head configs in _app from layout
    Signed-off-by: Mofajjal Rasul <imzihad@gmail.com>

--- components/Shared/Layout.tsx
+++ components/Shared/Layout.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Head from "next/head";
 import { Box, Container } from "@mui/material";
 import Footer from "@components/Shared/Footer";
 import Navbar from "@components/Shared/Navbar";
@@ -11,14 +10,6 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <React.Fragment>
-      <Head>
-        <title>Next.js App with TypeScript and Material-UI</title>
-        <meta
-          name="description"
-          content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vitae consectetur interdum, nisl nisi aliquam eros, eget egestas nisl nisi eget."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Box
         sx={{
           display: "flex",
